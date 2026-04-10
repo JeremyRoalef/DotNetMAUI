3573c7f15b149c692dbaee5ae2c219c9429bcb39
JeremyRoalef <roalef944@morrisville.edu>
2026-04-10 15:35:37 -0400

    Adding Final Lab Requirements
    This commit adds the final lab requirements. It was more guided than I would've liked, but it's completed.

--- MauiProfileApp/MauiProfileApp/MainPage.xaml.cs
+++ MauiProfileApp/MauiProfileApp/MainPage.xaml.cs
@@ -7,24 +7,54 @@
         public MainPage()
         {
             InitializeComponent();
+            showMessageSwitch.Toggled += OnShowMessageToggled;
+
+            //Initialize profile image visibility
+            profileImage.IsVisible = showMessageSwitch.IsToggled;
+        }
+
+        private void OnShowMessageToggled(object? sender, ToggledEventArgs e)
+        {
+            profileImage.IsVisible = showMessageSwitch.IsToggled;
         }
 
         void OnShowProfileClicked(object sender, EventArgs e)
         {
+            //Handle missing input
+            string errorMessage = "";
+
             if (string.IsNullOrEmpty(nameEntry.Text))
             {
-                resultLabel.Text = "You need to enter your name first";
+                errorMessage += "You need to enter your name first\n";
+            }
+            if (string.IsNullOrWhiteSpace(messageEntry.Text) || string.IsNullOrEmpty(messageEntry.Text))
+            {
+                errorMessage += "Please enter a message before continuing.";
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage) || !string.IsNullOrWhiteSpace(errorMessage))
+            {
+                //Invalid input found; display error message
+                resultLabel.Text = errorMessage;
                 return;
             }
 
-            string message = $"Hell {nameEntry.Text}";
+            string greetingOpener = greetingPicker.SelectedItem == null ? 
+                greetingPicker.Items.FirstOrDefault().ToString() : greetingPicker.SelectedItem.ToString();
+
+            string message = $"{greetingOpener}, {nameEntry.Text}";
             if (showMessageSwitch.IsToggled)
             {
-                message += $"/nMessage: {messageEntry.Text}";
+                message += $"\nMessage: {messageEntry.Text}";
             }
+
+            //Get the current date & display it
+            string currentDate = DateTime.Now.ToShortDateString();
+            message += $"\n{currentDate}";
 
             resultLabel.Text = message;
             resultLabel.FontSize = fontSlider.Value;
+            resultLabel.TextColor = Colors.Green;
         }
 
         void OnClearClicked(object sender, EventArgs e)
