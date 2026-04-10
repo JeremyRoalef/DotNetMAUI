94a92388c19574489047b1a260d4456815c1a522
JeremyRoalef <roalef944@morrisville.edu>
2026-04-10 15:02:21 -0400

    Add Lab Requirements
    This commit adds lab requirements. The C# and XAML code written was from the lab. Next commit will finish the lab, and I will add my own additions as per the lab requirements.

--- MauiProfileApp/MauiProfileApp/MainPage.xaml.cs
+++ MauiProfileApp/MauiProfileApp/MainPage.xaml.cs
@@ -9,16 +9,30 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        void OnShowProfileClicked(object sender, EventArgs e)
         {
-            count++;
+            if (string.IsNullOrEmpty(nameEntry.Text))
+            {
+                resultLabel.Text = "You need to enter your name first";
+                return;
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            string message = $"Hell {nameEntry.Text}";
+            if (showMessageSwitch.IsToggled)
+            {
+                message += $"/nMessage: {messageEntry.Text}";
+            }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            resultLabel.Text = message;
+            resultLabel.FontSize = fontSlider.Value;
+        }
+
+        void OnClearClicked(object sender, EventArgs e)
+        {
+            nameEntry.Text = string.Empty;
+            resultLabel.Text = string.Empty;
+            messageEntry.Text = string.Empty;
+            fontSlider.Value = 18;
         }
     }
 }
