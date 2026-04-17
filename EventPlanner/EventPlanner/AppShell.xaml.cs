bedb4090f4ebb776289b381b6dcc489437b5ac67
JeremyRoalef <roalef944@morrisville.edu>
2026-04-17 16:31:14 -0400

    Part 8: Flyout Items (But Actually Menu Items)

--- EventPlanner/EventPlanner/AppShell.xaml.cs
+++ EventPlanner/EventPlanner/AppShell.xaml.cs
@@ -12,5 +12,40 @@
             Routing.RegisterRoute(nameof(Confirmation), typeof(Confirmation));
             Routing.RegisterRoute(nameof(RegistrationConfirmation), typeof(RegistrationConfirmation));
         }
+
+        async void OnHomeClicked(object? sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"///{nameof(EventSelection)}");
+        }
+
+        async void OnEarthDayClicked(object? sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(EventDetails),
+                new Dictionary<string, object>
+                {
+                    {"ID", 2}
+                }
+                );
+        }
+
+        async void OnBookClubClicked(object? sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(EventDetails),
+                new Dictionary<string, object>
+                {
+                    {"ID", 1 }
+                }
+                );
+        }
+
+        async void OnAppliedStudentLearningClicked(object? sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(EventDetails),
+                new Dictionary<string, object>
+                {
+                    { "ID", 3}
+                }
+                );
+        }
     }
 }
